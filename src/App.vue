dcad17784a6599d185ad5e628b37389fe29f79b1
liuning <liuning@cennavi.com.cn>
2022-07-27 17:46:14 +0800

    fix: 修改pm2文件

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ import axios from 'axios'
 export default {
   name: 'App',
   created () {
-    axios.defaults.baseURL = 'http://172.21.149.151'
+    // axios.defaults.baseURL = 'http://172.21.149.151'
     axios({
       method: 'get',
       // url: 'http://172.21.149.151:8080/aaa'
